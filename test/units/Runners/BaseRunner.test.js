fcfcd80c54fd3b054f342f9080dd939d66ac766f
ChaselWu <minowu@foxmail.com>
2017-11-15 13:40:55 +0800

    Remove BaseRunner.test

--- test/units/Runners/BaseRunner.test.js
+++
@@ -1,9 +0,0 @@
-const BaseRunner = require('../../../src/Runners/BaseRunner')
-
-describe('BaseRunner.prototype.run()', () => {
-  test('setBaseWebpack', () => {
-    const runner = new BaseRunner()
-    const config = runner.run()
-    expect(config.resolve.extensions).toEqual(['.js', '.vue', '.json'])
-  })
-})
