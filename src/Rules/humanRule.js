21bc62217b71aa8c861222852e9d822142cd3bb7
ChaselWu <minowu@foxmail.com>
2017-11-17 15:28:45 +0800

    Add vue-human-env and strawes

--- src/Rules/humanRule.js
+++ src/Rules/humanRule.js
@@ -4,11 +4,15 @@ module.exports = runner => {
     loader: 'babel-loader',
     include: [
       runner.path.join(process.cwd(), 'node_modules/vue-human'),
-      runner.path.join(process.cwd(), 'node_modules/vue-human-icons')
+      runner.path.join(process.cwd(), 'node_modules/vue-human-icons'),
+      runner.path.join(process.cwd(), 'node_modules/vue-human-env'),
+      runner.path.join(process.cwd(), 'node_modules/strawes')
     ],
     exclude: [
       runner.path.join(process.cwd(), 'node_modules/vue-human/node_modules'),
       runner.path.join(process.cwd(), 'node_modules/vue-human-icons/node_modules')
+      runner.path.join(process.cwd(), 'node_modules/vue-human-env/node_modules'),
+      runner.path.join(process.cwd(), 'node_modules/strawes/node_modules')
     ]
   }
 }
