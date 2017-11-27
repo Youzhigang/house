631478a02872f0d22de38702cf31307ccf03673d
ChaselWu <minowu@foxmail.com>
2017-11-27 15:44:12 +0800

    Add setProxy and setAppEnvs

--- example/scripts/production.js
+++ example/scripts/production.js
@@ -2,4 +2,6 @@ const ProductionRunner = require('../../src/Runners/ProductionRunner')
 
 new ProductionRunner()
   .setOptions(require('./options'))
+  .setProxy(require('./proxyMaps'))
+  .setAppEnvs(require('./appEnvs'))
   .run()
--- example/scripts/server.js
+++ example/scripts/server.js
@@ -2,4 +2,6 @@ const ServerRunner = require('../../src/Runners/ServerRunner')
 
 new ServerRunner()
   .setOptions(require('./options'))
+  .setProxy(require('./proxyMaps'))
+  .setAppEnvs(require('./appEnvs'))
   .run()
