fdb58416ba98b9a128e205de26462e158f38baa1
ChaselWu <minowu@foxmail.com>
2017-11-27 19:30:00 +0800

    Add todo

--- example/scripts/options.js
+++ example/scripts/options.js
@@ -2,5 +2,7 @@ const path = require('path')
 
 module.exports = {
   moduleDirectory: path.join(process.cwd(), 'example'),
-  sassResources: path.join(process.cwd(), 'example/src/human/config.scss')
+  sassResources: path.join(process.cwd(), 'example/src/human/config.scss'),
+  // @todo Add multi example test to test publicPath
+  publicPath: '/house/'
 }
