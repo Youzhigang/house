d6fbf25f4ede07cb561bef64b715a236863de5a5
ChaselWu <minowu@foxmail.com>
2017-12-19 23:30:57 +0800

    Update sentry release value.

--- example/src/sentry/index.js
+++ example/src/sentry/index.js
@@ -5,7 +5,9 @@ import RavenVue from 'raven-js/plugins/vue'
 // It has sentry key, then install Raven
 if (process.env.SENTRY_KEY) {
   Raven
-    .config(process.env.SENTRY_KEY)
+    .config(process.env.SENTRY_KEY, {
+      release: process.env.GIT_COMMIT
+    })
     .addPlugin(RavenVue, Vue)
     .install()
 }
