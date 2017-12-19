90b2ac4410c31b72b139418c4a581e92dc86873f
ChaselWu <minowu@foxmail.com>
2017-12-19 23:27:34 +0800

    Add @todo

--- src/Plugins/Productions/sentryPlugin.js
+++ src/Plugins/Productions/sentryPlugin.js
@@ -2,6 +2,8 @@ const path = require('path')
 const SentryWebpackPlugin = require('@sentry/webpack-plugin')
 
 module.exports = runner => {
+  // @todo Check .sentryclirc extis in local root directory.
+  // If the file is missing, skip SentryWebpackPlugin.
   return new SentryWebpackPlugin({
     release () {
       return process.env.GIT_COMMIT
