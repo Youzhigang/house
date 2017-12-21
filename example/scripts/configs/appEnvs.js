0a11653f65ec0f81461b1113f148b17df8c59ce5
ChaselWu <minowu@foxmail.com>
2017-12-21 22:04:32 +0800

    Remove VERSION

--- example/scripts/configs/appEnvs.js
+++ example/scripts/configs/appEnvs.js
@@ -1,7 +1,4 @@
-const pkg = require('../../../package.json')
-
 module.exports = {
   NODE_ENV: 'development',
-  SENTRY_KEY: 'https://f282cc42e53e40409ca8f31190bcb6cd@sentry.io/257277',
-  VERSION: pkg.version
+  SENTRY_KEY: 'https://f282cc42e53e40409ca8f31190bcb6cd@sentry.io/257277'
 }
