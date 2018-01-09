3520f8b76b2d76cc2eccf55d9d35d34c5e3f8b39
ChaselWu <minowu@foxmail.com>
2018-01-09 23:21:25 +0800

    Add some for next-2

---
+++ scripts/development.js
@@ -0,0 +1,7 @@
+process.env.BUILT_PATH = 'example/dist'
+process.env.CACHE_PATH = 'example/cache'
+process.env.BUILT_INDEX_PATH = 'app'
+process.env.BUILT_INDEX_NAME = 'index.html' // 暂不开放
+process.env.BUILT_ASSET_PATH = 'static'     // 暂不开放
+process.env.SOURCE_PATH = 'example/src'
+process.env.SOURCE_INDEX_NAME = 'index.html'
