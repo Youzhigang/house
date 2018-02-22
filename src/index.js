921c0bfb253a3df0ea15e0de01b4356a7e96c958
ChaselWu <minowu@foxmail.com>
2018-02-22 12:24:21 +0800

    Update usage.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const DevelopmentRunner = require('./Runners/DevelopmentRunner')
 const ProductionRunner = require('./Runners/ProductionRunner')
 const ServerRunner = require('./Runners/ServerRunner')
 
-exports.DevelopmentRunner = DevelopmentRunner
-exports.ProductionRunner = ProductionRunner
-exports.ServerRunner = ServerRunner
+module.exports = {
+  DevelopmentRunner,
+  ProductionRunner,
+  ServerRunner
+}
