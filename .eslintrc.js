6f18686c1ca09dbbc0673fb7d67fce93cc491bab
ChaselWu <minowu@foxmail.com>
2018-05-14 11:45:27 +0800

    Add prettier format

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,15 +6,5 @@ module.exports = {
   env: {
     node: true
   },
-  extends: 'standard',
-  // add your custom rules here
-  'rules': {
-    'no-useless-escape': 0,
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': 0
-  }
+  extends: 'prettier'
 }
