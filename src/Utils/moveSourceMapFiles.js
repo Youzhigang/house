342eda2380980654f08307436dc9c6dd178ec5e1
youzhigang <youzhigang@suiyi.com.cn>
2018-05-16 13:18:24 +0800

    update console

--- src/Utils/moveSourceMapFiles.js
+++ src/Utils/moveSourceMapFiles.js
@@ -1,24 +1,26 @@
 const execSync = require('child_process').execSync
 const path = require('path')
 const fs = require('fs')
+const chalk = require('chalk')
 
 module.exports = function moveSourceMapFiles (builtDirectory) {
-  // const distPath = context.options.sourceMapDist
   const srcPath = path.join(builtDirectory, 'static/js')
   const destPath = path.join(builtDirectory, 'sourcemap')
   if (!fs.existsSync(destPath)) {
     console.log('创建sourcemap 目录')
     fs.mkdirSync(destPath)
-    // console.log(fs.existsSync(destPath))
+    
   }
-  // console.log(destPath, 'destPath1')
   const cmd = `mv ${srcPath}/*.map ${destPath}`
-  // console.log(cmd, 'mv sourcemap')
+
   try {
+    console.log(chalk.green('Begin to copy sourcemap'))
+    console.log(chalk.cyan(`Run cmd:  ${cmd}`))
     execSync(cmd)
   } catch (err) {
-    console.log('copy sourcemap error')
+    console.log('Copy sourcemap error')
   }
-  // console.log('destPath', destPath)
+  
+  console.log(chalk.green('Copy sourcemap complete!'))
   return destPath
 }
--- src/Utils/uploadSourceMapFiles.js
+++ src/Utils/uploadSourceMapFiles.js
@@ -8,12 +8,17 @@ module.exports = function uploadSourceMapFiles (builtDirectory) {
   try {
     const apikey = process.env.FUNDEBUG_API_KEY
     const version = process.env.VERSION_HASH
+    console.log(chalk.blue(`Please confirm the apikey ${apikey} and version ${version}`))
+    if (!apikey || !version) {
+      console.log(`The uploader stop, check apikey ${apikey} and version ${version}`)
+      return
+    }
     cmd = `fundebug-cli upload --apikey ${apikey} --appversion ${version} --directory ${builtDirectory}/sourcemap`
-    console.log(cmd, '--cmd')
-    console.log(chalk.green('begin to upload sourcemap...'))
+    console.log(chalk.cyan(`Run cmd:   ${cmd}`))
+    console.log(chalk.green('Begin to upload sourcemap...'))
     execSync(cmd)
-    console.log(chalk.green('upload sourcemap success!'))
+    console.log(chalk.green('Upload sourcemap success!'))
   } catch (err) {
-    console.log(chalk.red('upload sourcemap to fundebug fail...'))
+    console.log(chalk.red('Upload sourcemap to fundebug fail...'))
   }
 }
