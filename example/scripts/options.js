83b55c036923e9d98f67a023c400a1ea6ff40dae
youzhigang <youzhigang@suiyi.com.cn>
2018-05-16 13:53:03 +0800

    update example

--- example/scripts/options.js
+++ example/scripts/options.js
@@ -5,7 +5,6 @@ module.exports = {
   sassResources: path.join(process.cwd(), 'example/src/human/config.scss'),
   // @todo Add multi example test to test publicPath
   publicPath: './',
-  userErrorTrack: false,
   fundebug: {
     apiKey: '7069de8f71fe0ab7d8c9adaed971b0ff7febbda08a150f1f8fde0bfcd110886f'
   }
