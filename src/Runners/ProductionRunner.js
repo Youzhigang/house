83480f307026de84420f3f5d6e525ee802dd17be
ChaselWu <minowu@foxmail.com>
2018-05-16 20:30:03 +0800

    Add chunkhash for sourcemap file.

--- src/Runners/ProductionRunner.js
+++ src/Runners/ProductionRunner.js
@@ -34,7 +34,9 @@ class ProductionRunner extends BaseRunner {
           this.options.publicPath
         ),
         filename: this.parseAssetsFilename('js/[name].[chunkhash].js'),
-        sourceMapFilename: this.parseAssetsFilename('js/[name].js.map'),
+        sourceMapFilename: this.parseAssetsFilename(
+          'js/[name].[chunkhash].js.map'
+        ),
         chunkFilename: this.parseAssetsFilename('js/[id].[chunkhash].js')
       }
     })
